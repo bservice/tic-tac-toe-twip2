e90b9d31c9c4da2d625e82d48675b76496141bbf
bs666 <brservice@comcast.net>
2017-12-21 09:01:25 -0500

    wooooooo1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -15,13 +15,31 @@ public class Main {
         Scanner kb2 = new Scanner(System.in);
         int column1 = kb2.nextInt();
         String a [][] = new String [3][3];
-        a [row1][column1] = "X";
-        System.out.println("  " + a[row1][column1]+"  |       |     ");
-        System.out.println("___________________");
-        System.out.println("     |       |     ");
-        System.out.println("___________________");
-        System.out.println("     |       |     ");
+        a[0][0] = String.valueOf('-');
+        a[0][1] = String.valueOf('-');
+        a[0][2] = String.valueOf('-');
+        a[1][0] = String.valueOf('-');
+        a[1][1] = String.valueOf('-');
+        a[1][2] = String.valueOf('-');
+        a[2][0] = String.valueOf('-');
+        a[2][1] = String.valueOf('-');
+        a[2][2] = String.valueOf('-');
+        a [row1][column1] = String.valueOf('X');
+        Random rand = new Random();
+        Random rand2 = new Random();
+        int randRow = rand.nextInt(3);
+        int randCol = rand2.nextInt(3);
 
+        a [randRow][randCol] = String.valueOf('O');
+       for (int i = 0; i <= 2; i++)
+        {
+            for (int j= 0; j<=2; j++)
+            {
+                System.out.print(a[i][j]+ "\t" + "\t");
+            }
+            System.out.println();
+            System.out.println();
+        }
 
         /*if (move1.equals("2 3"))
         {
